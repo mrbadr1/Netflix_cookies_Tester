80b8454050ff36a3f57eeb80c0ba7e2e31180019
ABROUK BADRE <73764577+mrbadr1@users.noreply.github.com>
2023-05-22 22:41:46 +0200

    Update Cookie_Tester.py

--- Cookie_Tester.py
+++ Cookie_Tester.py
@@ -1,11 +1,12 @@
 import json,requests,sys,os
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QSizePolicy,QInputDialog,QMessageBox,QApplication, QComboBox ,QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QSizePolicy,QTextEdit,QDialog,QMessageBox,QApplication, QComboBox ,QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QFileDialog, QTableWidget, QTableWidgetItem, QHeaderView
 from PyQt5.QtGui import QPainter, QColor, QBrush, QFont
 from bs4 import BeautifulSoup
 from Functions import *
 
-class NetflixCookieTester(QWidget):
+
+class CookieTester(QWidget):
     def __init__(self):
         super().__init__()
 
@@ -34,8 +35,14 @@ class NetflixCookieTester(QWidget):
         layout.addWidget(self.import_button)
         
         self.import_button = QPushButton('Paste Cookies string', self)
-        self.import_button.clicked.connect(self.import_string)
+        self.import_button.clicked.connect(self.COOKIE_WINDOW_TOPAST)
         layout.addWidget(self.import_button)
+
+        self.result_label2 = QLabel('')
+        self.result_label2.setAlignment(Qt.AlignCenter)
+        self.result_label2.setFont(QFont("calibri", 11, QFont.Bold))
+        self.result_label2.setVisible(False)
+        layout.addWidget(self.result_label2)
 
         self.cookies_listbox = QComboBox(self)
         self.cookies_listbox.setGeometry(50, 70, 200, 30)
@@ -46,6 +53,8 @@ class NetflixCookieTester(QWidget):
         self.cookies_listbox.lineEdit().setReadOnly(True)
         self.cookies_listbox.lineEdit().setPlaceholderText("Select file...")
         self.cookies_listbox.currentIndexChanged.connect(self.on_cookies_listbox_changed)
+
+
         layout.addWidget(self.cookies_listbox)
         
         self.result_label = QLabel('')
@@ -54,7 +63,7 @@ class NetflixCookieTester(QWidget):
         layout.addWidget(self.result_label)
 
         self.account_button2 = QPushButton('convert NETSCAPE to JSON', self)
-        #self.account_button2.clicked.connect(self.convert_netscape_to_json)
+        self.account_button2.clicked.connect(lambda: self.COOKIE_WINDOW_TOCOPY(self.file_dict[self.cookies_listbox.currentText()]))
         self.account_button2.setVisible(False)
         layout.addWidget(self.account_button2)
 
@@ -72,13 +81,116 @@ class NetflixCookieTester(QWidget):
         layout.addWidget(self.account_details_table)
         self.setLayout(layout)
 
-    def import_string(self):
-        # Show the input dialog
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your string:')
+    def COOKIE_WINDOW_TOCOPY(self,PATH):
+        # Create a QDialog to display the JSON data
+        dialog = QDialog(self)
+        dialog.setWindowTitle('JSON Cookies')
+        layout = QVBoxLayout(dialog)
 
-        # If the user clicked OK, update the text box
-        if ok:
-            self.text_box.setText(text)
+        # Create a QTextEdit widget to display the JSON data
+        text_edit = QTextEdit(dialog)
+        text_edit.setReadOnly(True)
+        layout.addWidget(text_edit)
+
+        # Call the function to convert the Netscape file to JSON
+        json_cookies = self.convert_netscape_to_json(PATH)
+
+        # Insert the JSON data into the QTextEdit widget
+        text_edit.insertPlainText(json_cookies)
+
+        # Show the dialog
+        dialog.exec_()
+
+    def COOKIE_WINDOW_TOPAST(self):
+     dialog = QDialog(self)
+     dialog.setWindowTitle('JSON Cookies')
+     dialog.setFixedSize(450,390)
+     layout = QVBoxLayout(dialog)
+
+    # Create a QTextEdit widget to display the JSON data
+     self.text_edit = QTextEdit(dialog)
+     self.text_edit.setReadOnly(False)
+     self.text_edit.textChanged.connect(self.set_default_background_color)
+     layout.addWidget(self.text_edit)
+
+    # Create a button to test the cookies string
+     self.account_button3 = QPushButton('TEST COOKIES', self)
+     self.account_button3.clicked.connect(lambda: self.test_cookies_string(self.text_edit.toPlainText()))
+     layout.addWidget(self.account_button3)
+
+     self.result_label3 = QLabel('')
+     self.result_label3.setAlignment(Qt.AlignCenter)
+     self.result_label3.setFont(QFont("calibri", 11, QFont.Bold))
+     self.result_label3.setVisible(False)
+     layout.addWidget(self.result_label3)
+    # Show the dialog
+     dialog.exec_()
+
+
+
+    def set_default_background_color(self):
+        self.text_edit.setStyleSheet("")    
+    def test_cookies_string(self,string):
+     self.result_label3.setVisible(False)
+
+     try:
+      cookies_json = json.loads(string)
+      cookie_jar = json_cookies_to_cookiejar(cookies_json)
+
+      if not string.startswith(".") and "\t" not in string and test_netflix_cookies(cookie_jar):
+        # Set the background color of the text_edit widget to green
+        self.text_edit.setStyleSheet("background-color: green;")
+        self.result_label3.setVisible(True)
+        self.result_label3.setText("cookies working")
+      else:
+        # Reset the background color of the text_edit widget
+        self.text_edit.setStyleSheet("background-color: red;")
+        self.result_label3.setVisible(True)
+        self.result_label3.setText("cookies not working")
+     except json.decoder.JSONDecodeError as e:
+        self.text_edit.setStyleSheet("background-color: red;")
+        self.result_label3.setVisible(True)
+        self.result_label3.setText("format error")      
+
+           
+     except requests.exceptions.RequestException as e:
+                error_msg = f"Error connecting ..."
+                QMessageBox.critical(self, "Connection Error", error_msg)
+                return
+
+
+    def convert_netscape_to_json(self,file_path):
+     with open(file_path, "r+") as file:
+        netscape_data = file.read()
+     # Split the data into individual cookies
+        lines = netscape_data.strip().split('\n')
+     # Create an empty list to store the JSON data
+        cookies = []
+     # Loop through each cookie and add it to the JSON list
+        for line in lines:
+          if line.startswith('.netflix.com'):
+             parts = line.strip().split('\t')
+             cookie = {
+            'domain': parts[0],
+            'path': parts[2],
+            'name': parts[5],
+            'value': parts[6]
+             }
+             cookies.append(cookie)
+        json_cookies = json.dumps(cookies)
+
+        # Ask the user if they want to save the new JSON file in the original text file
+        reply = QMessageBox.question(self,'Save JSON file', 'Do you want to save the new JSON file in the original text file?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            # Write the JSON string back to the file
+            file.seek(0)
+            file.write(json_cookies)
+            file.truncate()
+
+        # Return the JSON string
+        return json_cookies
+
 
 
 
@@ -101,13 +213,17 @@ class NetflixCookieTester(QWidget):
             item_name = os.path.basename(file_name)
             self.file_dict[item_name] = file_name
             self.cookies_listbox.addItem(item_name)
+            index = self.cookies_listbox.findText(item_name)
             self.result_label.setText(f"")
             self.result_circle.set_color(QColor(0, 0, 0))
             self.account_button2.setVisible(False)
+            self.result_label2.setText(f"{len(file_names)} text files imported") # Set the text of result_label2 to show the number of imported text files
+            self.result_label2.setVisible(True)
 
               
     def check_cookies(self, item):
         try:
+            index = self.cookies_listbox.findText(item)
             file_name = self.file_dict[item]
             with open(file_name, 'r') as f:
               cookies_json = json.load(f)
@@ -117,20 +233,24 @@ class NetflixCookieTester(QWidget):
             if valid:
                 self.result_label.setText(f"cookies are working")
                 self.result_circle.set_color(QColor(0, 255, 0))
+                self.cookies_listbox.setItemData(index, QBrush(QColor("green")), Qt.BackgroundRole)
                 self.valid=True
                 self.account_button.setEnabled(True)
             else:
                 self.result_label.setText(f"cookies are not working")
+                self.cookies_listbox.setItemData(index, QBrush(QColor("red")), Qt.BackgroundRole)
                 self.account_button.setEnabled(False)
 
                 self.result_circle.set_color(QColor(255, 0, 0))
         except json.decoder.JSONDecodeError as e:
             if is_netscape_format(file_name):
              self.result_label.setText(f"the file is in netscape format ")
+             self.cookies_listbox.setItemData(index, QBrush(QColor("red")), Qt.BackgroundRole)
              self.account_button2.setVisible(True)
 
             else:
              self.result_label.setText(f"Error loading cookies from file ")
+             self.cookies_listbox.setItemData(index, QBrush(QColor("red")), Qt.BackgroundRole)
             self.account_button.setEnabled(False)
             self.result_circle.set_color(QColor(255, 0, 0))
             self.account_button.setEnabled(False)
@@ -139,7 +259,6 @@ class NetflixCookieTester(QWidget):
                 error_msg = f"Error connecting ..."
                 QMessageBox.critical(self, "Connection Error", error_msg)
                 return
-
 
     def scrape_account_details(self):
         item= self.cookies_listbox.currentText()
@@ -151,7 +270,6 @@ class NetflixCookieTester(QWidget):
         account_details = {}
         try:
                 file_name = self.file_dict[item]
-                print(file_name)
                 with open(file_name, 'r') as f:
                     cookies_json = json.load(f)
                 cookie_jar = json_cookies_to_cookiejar(cookies_json)
@@ -163,8 +281,7 @@ class NetflixCookieTester(QWidget):
                     account_details['Email'] = scrap(response.text, '<div data-uia="account-email" class="account-section-item account-section-email">', '</div>').strip()
                     account_details['Subscription plan'] = scrap(response.text, '<div class="account-section-item" data-uia="plan-label"><b>', '</b>').strip()
                     account_details['Member since'] = scrap(response.text, 'class="account-section-membersince--svg"></div>','</div>').strip()
-
-
+                    
         except requests.exceptions.RequestException as e:
                 error_msg = f"Error connecting to {url}: {e}"
                 QMessageBox.critical(self, "Connection Error", error_msg)
@@ -177,7 +294,6 @@ class NetflixCookieTester(QWidget):
                 return
 
         self.account_details = account_details
-        print(account_details)
         self.display_account_details()
 
     def display_account_details(self):
@@ -218,7 +334,7 @@ class CircleWidget(QWidget):
 
 def main():
     app = QApplication(sys.argv)
-    window = NetflixCookieTester()
+    window = CookieTester()
     window.show()
     sys.exit(app.exec_())
 
